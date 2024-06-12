5d67b2c73b5b1647e07f1c9e32a6d4a600d220ca
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2024-06-12 22:06:43 +0530

    creating backend

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const mongoURL: string = process.env.MONGO_URL ?? "";
+
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((error) => {
+    console.log("Error when connecting the database : ", error);
+    console.log("ddd ", mongoURL);
+  });
